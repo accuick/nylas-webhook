2fac8ae7d07fbc9119e6ad50583c357ac59d0533
Aditya Kumar Togaru <adityak@askconsulting.com>
2023-02-28 19:05:29 +0530

    request body JSON

--- src/index.js
+++ src/index.js
@@ -26,6 +26,8 @@ app.all("/", (request, response) => {
     if (request.method === "POST") {
         console.log('==========BODY DELTAS START==========');
         console.log(request.body);
+        console.log("parsed data");
+        console.log(JSON.parse(request.body));
         if (request.body.deltas[0].metadata) {
             for (const key in request.body.deltas[0].metadata) {
                 console.log(key + ": " + request.body.deltas[0].metadata[key])
@@ -36,5 +38,9 @@ app.all("/", (request, response) => {
 
     response.status(200).end() // Responding is important
 })
+
+// https://api.render.com/deploy/srv-cfusp3ta499aogr9h8tg?key=02TFc8rz4vI
+
+// https://search.accuick.com/Twilio/webhook_nylas.jsp
 
 app.listen(3000);
